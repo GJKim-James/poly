94d52b1b8f49dbba45cf013dbdf04ec0d3f28f17
data03 <https://github.com/GJKim-James/poly>
2021-03-24 11:35:34 +0900

    3월 24일 프로그래밍 언어 실습 수업

---
+++ GjkimPrj/src/Ex03_14.java
@@ -0,0 +1,18 @@
+
+public class Ex03_14 {
+
+	public static void main(String[] args) {
+		
+		boolean boo1, boo2;
+		
+		boo1 = true;
+		
+		System.out.println("boo1 : " + boo1);
+		
+		boo2 = (10 == 20);
+		
+		System.out.println("boo2 : " + boo2);
+
+	}
+
+}
---
+++ GjkimPrj/src/Ex05_01.java
@@ -0,0 +1,16 @@
+
+public class Ex05_01 {
+
+	public static void main(String[] args) {
+		
+		int a = 99; // a = 99가 아니면 "난 무조건 실행된다."만 출력
+		
+		if (a < 100) {
+			System.out.println("100보다 작군요.");
+		}
+		
+		System.out.println("난 무조건 실행된다.");
+			
+	}
+
+}
