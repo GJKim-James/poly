8be61f8d2986925fe37537052a6c572dc4e60021
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-03-24 12:13:11 +0900

    3월 24일 프로그래밍 언어 실습 수업

---
+++ Ex04_03.java
@@ -0,0 +1,43 @@
+
+public class Ex04_03 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+	
+		a++;
+		System.out.println("a : " + a);
+		System.out.println("a++ : " + a++);
+				
+		System.out.println("a : " + a);
+		System.out.println("++a : " + ++a);
+		System.out.println();
+
+		a--;
+		System.out.println("a : " + a);
+		System.out.println();
+		
+		a += 5;
+		System.out.println("a : " + a);
+		System.out.println();
+		
+		a -= 5;
+		System.out.println("a : " + a);
+		System.out.println();
+		
+		a *= 5;
+		System.out.println("a : " + a);
+		System.out.println();
+		
+		a /= 5;
+		System.out.println("a : " + a);
+		System.out.println();
+		
+		a %= 5;
+		System.out.println("a : " + a);
+		System.out.println();
+		
+	}
+
+}
