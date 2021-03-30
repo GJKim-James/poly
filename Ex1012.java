a19942fece430eff20d9877b9fe5e98045f06804
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-03-30 12:27:30 +0900

    3월 30일 CodeUp

---
+++ Ex1012.java
@@ -0,0 +1,15 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1012 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		float f = s.nextFloat();
+		System.out.printf("%f", f);
+
+	}
+
+}
