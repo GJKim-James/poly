30ef28b4f0c631a8c883c350d431d5814ec94507
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-04-01 13:19:12 +0900

    4월 1일 CodeUp

---
+++ Ex1014.java
@@ -0,0 +1,20 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1014 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		char a, b;
+		
+		a = s.next().charAt(0);
+		b = s.next().charAt(0);
+		
+		System.out.printf("%c %c", b, a);
+
+	}
+
+}
