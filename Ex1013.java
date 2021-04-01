410c62c3e190a0a0b4054c3c4f207566b6680e13
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-04-01 13:19:40 +0900

    4월 1일 CodeUp

---
+++ Ex1013.java
@@ -0,0 +1,20 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1013 {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		
+		Scanner s = new Scanner(System.in);
+		
+		a = s.nextInt();
+		b = s.nextInt();
+		
+		System.out.println(a + " " + b);
+
+	}
+
+}
