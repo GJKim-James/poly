969eb0a1d6568dc2c65e1d0b35cec61dc56412b0
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-04-02 12:16:10 +0900

    4월 2일 CodeUp

---
+++ Ex1015.java
@@ -0,0 +1,19 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1015 {
+
+	public static void main(String[] args) {
+		
+		float a;
+		
+		Scanner s = new Scanner(System.in);
+		
+		a = s.nextFloat();
+		
+		System.out.printf("%.2f", a);
+
+	}
+
+}
