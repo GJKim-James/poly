6dc67f73c50fb9e860237c84b564b95f4ac6276a
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-04-05 12:45:38 +0900

    4월 5일 CodeUp

---
+++ CodeUp/src/problemset/Ex1017.java
@@ -0,0 +1,19 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1017 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		
+		Scanner s = new Scanner(System.in);
+		
+		a = s.nextInt();
+		
+		System.out.println(a + " " + a + " " + a);
+
+	}
+
+}
