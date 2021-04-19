362a46dff8dd5d8228342858e091e9919388f370
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-04-20 08:59:57 +0900

    4월 20일 CodeUp

---
+++ CodeUp/src/problemset/Ex1021.java
@@ -0,0 +1,19 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1021 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.printf("입력하고 싶은 단어를 쓰세요 : ");
+		
+		String a = s.nextLine();
+		
+		System.out.println(a);
+
+	}
+
+}
