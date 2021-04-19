bfe9f299cb087d530f4a3ab03fbff5f97fa3e72e
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-04-19 10:34:25 +0900

    4월 19일 자바 예제모음 06

---
+++ GjkimPrj/src/Example06.java
@@ -0,0 +1,15 @@
+
+public class Example06 {
+
+	public static void main(String[] args) {
+		
+		System.out.printf("byte 형의 크기 : %d bit \n", Byte.SIZE);
+		System.out.printf("short 형의 크기 : %d bit \n", Short.SIZE);
+		System.out.printf("int 형의 크기 : %d bit \n", Integer.SIZE);
+		System.out.printf("long 형의 크기 : %d bit \n", Long.SIZE);
+		System.out.printf("float 형의 크기 : %d bit \n", Float.SIZE);
+		System.out.printf("double 형의 크기 : %d bit \n", Double.SIZE);
+		System.out.printf("char 형의 크기 : %d bit \n", Character.SIZE);
+	}
+
+}
