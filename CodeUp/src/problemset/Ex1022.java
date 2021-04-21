8c2bc7344398546581ac0e3b1ceff23f0e2cf796
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-04-21 11:30:01 +0900

    4월 21일 CodeUp

---
+++ CodeUp/src/problemset/Ex1022.java
@@ -0,0 +1,18 @@
+
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1022 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		String str = s.nextLine();
+		
+		System.out.println(str);
+
+	}
+
+}
