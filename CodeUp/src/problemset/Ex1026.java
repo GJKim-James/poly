cee60de9c496248382bb57ba6605dcbbbefcb211
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-05-11 11:57:01 +0900

    5월 11일 CodeUp

---
+++ CodeUp/src/problemset/Ex1026.java
@@ -0,0 +1,17 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1026 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		String tmp[] = s.next().split(":");
+		
+		System.out.println(Integer.parseInt(tmp[1]));
+
+	}
+
+}
