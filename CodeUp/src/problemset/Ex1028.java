6e0b5c6a77a493b9de4a5d462b6cfffddc8616eb
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-05-17 10:00:16 +0900

    5월 17일 CodeUp

---
+++ CodeUp/src/problemset/Ex1028.java
@@ -0,0 +1,17 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1028 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		long a = s.nextLong();
+		
+		System.out.println(a);
+
+	}
+
+}
