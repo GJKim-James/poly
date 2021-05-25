725991f2671db75ee0f926b88ae9545f2fb1a10e
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-05-25 11:29:15 +0900

    5월 25일 CodeUp

---
+++ CodeUp/src/problemset/Ex1038.java
@@ -0,0 +1,20 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1038 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		long a = s.nextLong();
+		
+		long b = s.nextLong();
+
+		long sum = a + b;
+		System.out.println(sum);
+
+	}
+
+}
