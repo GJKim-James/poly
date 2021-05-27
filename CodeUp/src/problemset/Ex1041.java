ed9512ee6615d67a09f68f74fe16284e9110ba88
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-05-27 12:29:51 +0900

    5월 27일 CodeUp

---
+++ CodeUp/src/problemset/Ex1041.java
@@ -0,0 +1,17 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1041 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		char a = s.next().charAt(0);
+		
+		System.out.printf("%c", (a + 1));
+		
+	}
+
+}
