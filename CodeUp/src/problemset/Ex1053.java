824a0701f7e8c1f61495dcc73ea05b304f0b7ae8
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-06-08 10:47:06 +0900

    6월 8일 CodeUp

---
+++ CodeUp/src/problemset/Ex1053.java
@@ -0,0 +1,21 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1053 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+
+		if (a == 0) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+
+	}
+
+}
