5fdd290d37b8e5ab2b25075ede3c366ef5c475bb
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-06-11 09:55:38 +0900

    6월 11일 CodeUp

---
+++ CodeUp/src/problemset/Ex1057.java
@@ -0,0 +1,22 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1057 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+
+		if (a == b) {
+			System.out.print("1");
+		} else {
+			System.out.print("0");
+		}
+
+	}
+
+}
---
+++ CodeUp/src/problemset/Ex1058.java
@@ -0,0 +1,22 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1058 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+
+		if (a == 0 && b == 0) {
+			System.out.print("1");
+		} else {
+			System.out.print("0");
+		}
+
+	}
+
+}
