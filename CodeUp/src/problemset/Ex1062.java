bfd5ec52a95229ef8047a71d2a32bb34983cf8b0
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2021-06-18 11:57:22 +0900

    6월 18일 CodeUp

---
+++ CodeUp/src/problemset/Ex1062.java
@@ -0,0 +1,18 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1062 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+		int b = s.nextInt();
+
+		System.out.println((a ^ b));
+
+	}
+
+}
