50dc05de9b020dc7dde85b8f1086c1ae0fd289a1
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2022-03-15 22:38:35 +0900

    2022.03.15 CodeUp

---
+++ CodeUp/src/problemset/Ex1063.java
@@ -0,0 +1,19 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1063 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		int a = s.nextInt();
+	
+		int b = s.nextInt();
+
+		System.out.print(a > b ? a : b);
+
+	}
+
+}
