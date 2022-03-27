ff103995f84bff5f9fb5a0d08991ba67b49653d8
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2022-03-27 20:16:13 +0900

    2022. 03. 27 CodeUp

---
+++ CodeUp/src/problemset/Ex1072.java
@@ -0,0 +1,25 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1072 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		int a[] = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			
+			a[i] = sc.nextInt();
+			
+			System.out.println(a[i]);
+			
+		}
+
+	}
+
+}
