5209b419be69ed54040529e10ed900fc7b5ee5f4
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2022-03-28 23:54:17 +0900

    2022. 03. 28 CodeUp

---
+++ CodeUp/src/problemset/Ex1073.java
@@ -0,0 +1,26 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1073 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+
+			int n = sc.nextInt();
+
+			if (n != 0) {
+				
+				System.out.println(n);
+				
+			} else {
+				
+				break;
+			}
+		}
+	}
+
+}
