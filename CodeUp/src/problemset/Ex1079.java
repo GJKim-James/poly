5720d639fd183f9d004b2648cec12d4f9eb8040f
GJKim-James <80372047+GJKim-James@users.noreply.github.com>
2022-04-10 23:03:08 +0900

    2022. 04. 10 CodeUp

---
+++ CodeUp/src/problemset/Ex1079.java
@@ -0,0 +1,25 @@
+package problemset;
+
+import java.util.Scanner;
+
+public class Ex1079 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			
+			char c = sc.next().charAt(0);
+			
+			System.out.println(c);
+			
+			if (c == 'q') {
+				break;
+			}
+			
+		}
+		
+	}
+
+}
